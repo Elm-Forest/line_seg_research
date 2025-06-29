2f9bfe41e9bc6f992facfde926a738dc6b758be9
Elm Forest <timbrightland@163.com>
2025-06-29 13:38:52 +0800

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 pip install -r requirements.txt
+pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu124
+
 
 python models/deep_hough_transform/dht_module/_cdht/setup.py install
-
 python models/deep_hough_transform/idht_module/_ciht/setup.py install
